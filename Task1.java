8f6293244d7293ff0f059e4ea490fb4ca6147b3f
Eugene Khodakov <khodakow@gmail.com>
2017-11-23 15:49:36 +0300

    Add files via upload

---
+++ Task1.java
@@ -0,0 +1,13 @@
+package com.company.Task1;
+
+import java.io.*;
+import java.io.BufferedReader;
+
+public class Task1 {
+    public static void main(String args[]){
+        Controller controller = new Controller();
+        controller.commandLine();
+    }
+}
+
+
